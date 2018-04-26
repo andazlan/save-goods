5e6f47776de2d5506543a47375d487efca059dab
Andrew Azlan <azlanappleboy@gmail.com>
2018-04-26 12:07:55 +0700

    save goods to db

--- app/src/main/java/com/andazlan/goods/DetailActivity.java
+++ app/src/main/java/com/andazlan/goods/DetailActivity.java
@@ -8,25 +8,42 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.andazlan.goods.model.Good;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DetailActivity extends AppCompatActivity {
     private TextView name, description, stock;
     private Spinner category;
     private Button save;
+
+    private FirebaseDatabase mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        mDatabase = FirebaseDatabase.getInstance();
 
         setTitle(getString(R.string.label_new_good));
 
         name = findViewById(R.id.edt_name);
         description = findViewById(R.id.edt_description);
         stock = findViewById(R.id.edt_stock);
+        category = findViewById(R.id.spn_category);
         save = findViewById(R.id.btn_save);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(DetailActivity.this, "Save to fb", Toast.LENGTH_SHORT).show();
+                String id = mDatabase.getReference("goods").push().getKey();
+                Good newGood = new Good(id,
+                        name.getText().toString(),
+                        description.getText().toString(),
+                        category.getSelectedItem().toString(),
+                        Integer.valueOf(stock.getText().toString()));
+                mDatabase.getReference().child("goods").child(id).setValue(newGood);
+                finish();
+                Toast.makeText(DetailActivity.this, "Data saved", Toast.LENGTH_SHORT).show();
             }
         });
     }
